bae5aff17329f7c87863172cc5a7fcd879e33a07
FANTAS666IXI <fantas666x@hotmail.com>
2024-05-19 20:39:00 +0200

    F6X THE GOLDEN TOUCH 0.6.0
    Add Highlight Selected Screen Feature
    Update ScreensMenu.cs

--- F6X THE GOLDEN TOUCH/Assets/Scripts/Screens/ScreensMenu.cs
+++ F6X THE GOLDEN TOUCH/Assets/Scripts/Screens/ScreensMenu.cs
@@ -1,14 +1,65 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScreensMenu : MonoBehaviour
 {
-    public GameObject[] screens;
+    [Header("Screens Menu Parameters")]
+    public Color currentScreenColor;
+
+    private int currentScreen;
+    private GameObject[] screens;
+    private GameObject[] screensButtons;
+
+    private void Awake()
+    {
+        InitializeComponents();
+        InitializeVariables();
+    }
+
+    private void InitializeComponents()
+    {
+        InitializeScreens();
+        InitializeScreensButtons();
+    }
+
+    private void InitializeScreens()
+    {
+        screens = new GameObject[6];
+        for (int i = 0; i <= 5; i++)
+            screens[i] = transform.parent.transform.GetChild(i).gameObject;
+
+    }
+
+    private void InitializeScreensButtons()
+    {
+        screensButtons = new GameObject[6];
+        for (int i = 0; i <= 5; i++)
+            screensButtons[i] = transform.GetChild(i).gameObject;
+    }
+
+    private void InitializeVariables()
+    {
+        currentScreen = 2;
+    }
 
     private void ActiveScreen(int screenNumber)
     {
-        foreach (GameObject screen in screens)
-            screen.SetActive(false);
-        screens[screenNumber].SetActive(true);
+        currentScreen = screenNumber;
+        for (int i = 0; i <= 5; i++)
+        {
+            if (i == screenNumber)
+            {
+                screens[i].SetActive(true);
+                screensButtons[i].GetComponent<Image>().color = currentScreenColor;
+                screensButtons[i].GetComponent<Button>().enabled = false;
+            }
+            else
+            {
+                screens[i].SetActive(false);
+                screensButtons[i].GetComponent<Image>().color = Color.white;
+                screensButtons[i].GetComponent<Button>().enabled = true;
+            }
+        }
     }
 
     public void ClickSkinsScreenButton()
--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # _F6X THE GOLDEN TOUCH_
 
-![THUMBNAIL](Resources/Img/MainScreen.png)
+![THUMBNAIL](Resources/Img/MainScreen.png) 
 ![THUMBNAIL](Resources/Img/UpgradesScreen.png)
-![THUMBNAIL](Resources/Img/StatsScreen.png)
+
+![THUMBNAIL](Resources/Img/StatsScreen.png) 
 ![THUMBNAIL](Resources/Img/AchievementsScreen.png)
-![THUMBNAIL](Resources/Img/SkinsScreen.png)
+
+![THUMBNAIL](Resources/Img/SkinsScreen.png) 
 ![THUMBNAIL](Resources/Img/OptionsScreen.png)
 
 - **Description :**
@@ -18,7 +20,7 @@
 ---
 
 - **Stack :**
-  - **F6X The Golden Touch** : `0.5.0`
+  - **F6X The Golden Touch** : `0.6.0`
   - **Android** : `8.0`
   - **Unity** : `2022.3.20f1`
   - **Console Log System** : `1.3.0`
